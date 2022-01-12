19e843ebf1f0e2abbee5c4502b39bfdab5755a1e
Mathias Staricka <mstaricka@gmail.com>
2022-01-12 14:44:07 -0800

    day 7 refactoring and optimization

--- src/day7.rs
+++ src/day7.rs
@@ -4,63 +4,70 @@ use crate::day::Day;
 
 pub struct Day7 {}
 
-fn increasing_cost(a: i32, b: i32) -> i32 {
-    let mut cost = 0;
-    for i in 1..(a-b).abs()+1 {
-        cost += i;
+trait Cost {
+    fn cost(&mut self, a: i32, b: i32) -> i32;
+}
+
+struct BasicCost {}
+
+impl Cost for BasicCost {
+    fn cost(&mut self, a: i32, b: i32) -> i32 {
+        (a - b).abs()
     }
-    cost
+}
+
+struct IncreasingCostCache {
+    cache: Vec<i32>
+}
+
+impl IncreasingCostCache {
+    fn new() -> IncreasingCostCache {
+        IncreasingCostCache {cache: vec![0]}
+    }
+}
+
+impl Cost for IncreasingCostCache {
+    fn cost(&mut self, a: i32, b: i32) -> i32 {
+        let dist = (a-b).abs() as usize;
+        while self.cache.len() < dist + 1 {
+            self.cache.push(self.cache[self.cache.len() - 1] + self.cache.len() as i32);
+        }
+        self.cache[dist]
+    }
+}
+
+fn compute_min_cost(input: &str, cost_method: &mut dyn Cost) -> String {
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+    let mut counts = HashMap::new();
+
+    for pos in input.lines().next().unwrap().split(",").map(|n| n.parse().unwrap()) {
+        min = cmp::min(min, pos);
+        max = cmp::max(max, pos);
+        match counts.get_mut(&pos) {
+            None => {counts.insert(pos, 1);},
+            Some(c) => *c += 1
+        }
+    }
+
+    let mut best = i32::MAX;
+    for i in min..max {
+        let mut cost = 0;
+        for (pos, count) in counts.iter() {
+            cost += count * cost_method.cost(i, *pos);
+        }
+        best = cmp::min(best, cost);
+    }
+    best.to_string()
 }
 
 impl Day for Day7 {
     fn part1(&self, input: &str) -> String {
-        let mut min = i32::MAX;
-        let mut max = i32::MIN;
-        let mut counts = HashMap::new();
-
-        for pos in input.lines().next().unwrap().split(",").map(|n| n.parse().unwrap()) {
-            min = cmp::min(min, pos);
-            max = cmp::max(max, pos);
-            match counts.get_mut(&pos) {
-                None => {counts.insert(pos, 1);},
-                Some(c) => *c += 1
-            }
-        }
-
-        let mut best = i32::MAX;
-        for i in min..max {
-            let mut cost = 0;
-            for (pos, count) in counts.iter() {
-                cost += count * (i - pos).abs();
-            }
-            best = cmp::min(best, cost);
-        }
-        best.to_string()
+        compute_min_cost(input, &mut BasicCost{})
     }
 
     fn part2(&self, input: &str) -> String {
-        let mut min = i32::MAX;
-        let mut max = i32::MIN;
-        let mut counts = HashMap::new();
-
-        for pos in input.lines().next().unwrap().split(",").map(|n| n.parse().unwrap()) {
-            min = cmp::min(min, pos);
-            max = cmp::max(max, pos);
-            match counts.get_mut(&pos) {
-                None => {counts.insert(pos, 1);},
-                Some(c) => *c += 1
-            }
-        }
-
-        let mut best = i32::MAX;
-        for i in min..max {
-            let mut cost = 0;
-            for (pos, count) in counts.iter() {
-                cost += count * increasing_cost(i, *pos);
-            }
-            best = cmp::min(best, cost);
-        }
-        best.to_string()
+        compute_min_cost(input, &mut IncreasingCostCache::new())
     }
 }
 
